218cab10ecf6ae38e032fc64e8eda4cb1b282dc7
Rubén Cayuelas (RCA) <165658337+RubenCayuelas@users.noreply.github.com>
2025-06-21 20:56:15 +0200

    Refactor App component and add styling for HomePage; include new fonts and background image

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import instagram_logo from 'assets/images/instagram_logo.svg';
+import 'assets/styles/HomePage.css';
 
 export function HomePage() {
   return (
     <div data-testid="homepage">
-      <h1>rcyux_</h1>
+      <div className="background">
+        <h1>
+          <img src={instagram_logo} className="instagram-logo" alt="Instagram Logo" />
+          rcyux_
+        </h1>
+      </div>
+      <div className="content">
+        <h2>Hello World</h2>
+      </div>
     </div>
   );
 }
